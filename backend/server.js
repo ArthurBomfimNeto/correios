0ca85f4dd9bc63c93ece64259115df4dd68736fe
Arthur Bomfim Neto <you@example.com>
2020-10-31 19:00:43 -0300

    Alterado a rota frete para utilizar como post

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
 // cria um objeto da dependencia do express
 const express = require('express');
-
 //vamo criar o server do express 
-const server = express();
+const app = express();
+//porta que o servidor vai ouvir
+const port = process.env.PORT || 3001;
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended : false }))
+app.use(bodyParser.json())
 //criar um objeto da dependencia do cors - segurança 
 const cors = require('cors');
+app.use(cors());
 //importando para dependencia node-correios
 const Correios = require('node-correios')
 //instanciar para um objeto  variavel 
 
 const correios = new Correios()
 
-server.use(cors());
 
-server.get('/cep/:cep', (req, res) => {
-    let user = req.params.cep // recuperar o que usuario envia como parametro 
-    console.log(user)
-    correios.consultaCEP({cep: user})
+
+app.get('/cep/:cep', (req, res) => {
+    let x = req.params.cep // recuperar o que usuario envia como parametro 
+    console.log(x)
+    correios.consultaCEP({cep: x})
     .then(result => {
         console.log(result)
         res.json(result) //devolve para o front end 
@@ -27,18 +32,18 @@ server.get('/cep/:cep', (req, res) => {
     })
 });
 
-server.get('/frete/', (req,res) => {
-
+app.post('/frete', (req,res) => {
+    console.log(req.body)
     const args = {
-        nCdServico: '04014',
-        sCepOrigem: '14401295',
-        sCepDestino: '14403646',
-        nVlPeso: "1",
-        nCdFormato: 10,
-        nVlComprimento: 10,
-        nVlAltura: 10,
-        nVlLargura: 10,
-        nVlDiametro: 10
+        nCdServico: req.body.servico,
+        sCepOrigem: req.body.origem,
+        sCepDestino:req.body.destino,
+        nVlPeso: req.body.peso,
+        nCdFormato: req.body.formato, 
+        nVlComprimento: req.body.comp,
+        nVlAltura: req.body.altura,
+        nVlLargura: req.body.largura,
+        nVlDiametro: req.body.diametro
     }
 
     correios.calcPreco(args)
@@ -52,15 +57,12 @@ server.get('/frete/', (req,res) => {
 
 
 
-//porta que o servidor vai ouvir
-const port = process.env.PORT || 3001;
-
 //vamos definir uma rota com método get 
-server.get('/cep',(req, res, next) => {
+app.get('/cep',(req, res, next) => {
     res.send("Aplicação em node com os Correios");
 })
 
 //sobe o sevidor - colocar para ouvir 
-server.listen(port, () =>{
+app.listen(port, () =>{
     console.log(`Servidor rodando na porta $ ${port}`)
 });
