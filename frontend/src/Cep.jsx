dff3b49d3d82ed1c2fee0f0f6d2a1726006bfdb2
Arthur Bomfim Neto <you@example.com>
2020-10-31 19:02:27 -0300

    Alterado para receber parametros do calculo do frete

--- frontend/src/Cep.jsx
+++ frontend/src/Cep.jsx
@@ -8,23 +8,46 @@ export default class Cep extends Component {
     constructor(){
         super()
         this.state = {
-            cep:'',
-            endereco:''
+            frete:{
+                origem:'',
+                destino:'',
+                servico:'',
+                peso:'',
+                formato:'',
+                comp:'',
+                altura:'',
+                largura:'',
+                diametro:''
+            },
+            resultado:'',
+            resultado2:''
         }
     }
 
-    setCep(e){
-        this.setState({
-            cep: e.target.value
-        })
+    setFrete(e){
+        e.persist();
+        let value = e.target.value;
+
+        this.setState(prevState => ({
+            frete: { ...prevState.frete, [e.target.name]: value}
+        }))
     }
-    consultaCep(){
-        const url =`http://localhost:3001/cep/${this.state.cep}`
+
+    chama(){
+        const url =`http://localhost:3001/cep/${this.state.frete.origem}`
         axios.get(url)
         .then(resposta => {
-            this.setState({endereco: resposta.data})// atualiza o endereco 
+            this.setState({resultado: resposta.data})// atualiza o endereco 
+            console.log(resposta)
         })
-        alert(`OK ${this.state.cep}`)
+    }
+
+    fretar(){
+
+        axios.post(`http://localhost:3001/frete`, this.state.frete)
+        .then(resposta => {
+            console.log(resposta.data[0]); this.setState({resultado2: resposta.data[0]})
+        })
     }
     render() {
         return (
@@ -32,18 +55,69 @@ export default class Cep extends Component {
                 <h2>Exemplo de Consulta ao CEP </h2>
                 <form>
                     <div className="form-group">
-                         <input value={this.state.cep} onChange={e => this.setCep(e)} type="number" className="form-control"/>
+                        <label> CEP </label>
+                        <input type="number" className="form-control" name="origem" value={this.state.frete.origem} onChange={e => this.setFrete(e)}/>
                     </div>
                     <div className="form-group">
-                        <button type ="button" onClick={ e => {this.consultaCep()}} className="btn btn-primary"> Consulta </button>
+                        <button type ="button" onClick={ e => this.chama()} className="btn btn-primary"> Consulta </button>
                     </div>
                     <div className="form-group">
                         <ul>
-                            <li> Logradouro : {this.state.endereco.logradouro}</li>
-                            <li> Bairro : {this.state.endereco.bairro}</li>
-                            <li> Localidade : {this.state.endereco.localidade}</li>
-                            <li> UF : {this.state.endereco.uf}</li>
-                            <li> DDD : {this.state.endereco.ddd}</li>
+                            <li> Logradouro : {this.state.resultado.logradouro}</li>
+                            <li> Bairro : {this.state.resultado.bairro}</li>
+                            <li> Localidade : {this.state.resultado.localidade}</li>
+                            <li> UF : {this.state.resultado.uf}</li>
+                            <li> DDD : {this.state.resultado.ddd}</li>
+                        </ul>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Destino</label>
+                        <input type="number" className="form-control" name="destino" value={this.state.frete.destino} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Serviço</label>
+                        <input type="number" className="form-control" name="servico" value={this.state.frete.servico} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Peso</label>
+                        <input type="number" className="form-control" name="peso" value={this.state.frete.peso} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Formato</label>
+                        <input type="number" className="form-control" name="formato" value={this.state.frete.formato} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Comprimento</label>
+                        <input type="number" className="form-control" name="comp" value={this.state.frete.comp} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Altura</label>
+                        <input type="number" className="form-control" name="altura" value={this.state.frete.altura} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Largura</label>
+                        <input type="number" className="form-control" name="largura" value={this.state.frete.largura} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Diâmetro</label>
+                        <input type="number" className="form-control" name="diametro" value={this.state.frete.diametro} onChange={e => this.setFrete(e)}/>
+                    </div>
+
+                    <div className='form-group'>
+                        <button className="btn btn-primary" type="button" onClick={e => this.fretar()}>Consulta</button>
+                    </div>
+
+                    <div className="form-group">
+                        <ul>
+                            <li> Valor: {this.state.resultado2.Valor}</li>
                         </ul>
                     </div>
                 </form>
